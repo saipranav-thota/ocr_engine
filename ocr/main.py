53ffcb18d73975c74c0766729d986ee9f212a350
saipranav-thota <134518127+saipranav-thota@users.noreply.github.com>
2025-06-12 15:26:25 +0530

    Delete ocr directory

--- ocr/main.py
+++
@@ -1,7 +0,0 @@
-from ocr_extraction import analyze_image_from_url
-from upload import upload_image_to_blob
-
-
-
-local_image_path = "handwritten.png"
-analyze_image_from_url(upload_image_to_blob(local_image_path))
